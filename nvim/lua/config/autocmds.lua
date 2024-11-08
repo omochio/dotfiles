276f39ee95a6623cdc67bf6c2e9210639c45f6e9
omochio <omochiodev@gmail.com>
2024-11-08 09:31:21 +0900

    tab and tree option

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local filetype_tabstop = { lua = 2, markdown = 2, c = 4, cpp = 4 } -- filetype毎のインデント幅
+local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = usrftcfg,
+  callback = function(args)
+    local ftts = filetype_tabstop[args.match]
+    if ftts then
+      vim.bo.tabstop = ftts
+      vim.bo.shiftwidth = ftts
+    end
+  end,
+})
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+local filetype_tabstop = { lua = 2, markdown = 2, c = 4, cpp = 4 } -- filetype毎のインデント幅
+local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = usrftcfg,
+  callback = function(args)
+    local ftts = filetype_tabstop[args.match]
+    if ftts then
+      vim.bo.tabstop = ftts
+      vim.bo.shiftwidth = ftts
+    end
+  end,
+})
